7b5f4b38401b60875fe84459f5c239f56fe2a0bb
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-13 03:12:28 +0100

    repository

---
+++ src/main/java/com/akh/SpringSecurity/repositories/AppUserRepository.java
@@ -0,0 +1,10 @@
+package com.akh.SpringSecurity.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.akh.SpringSecurity.models.AppUser;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+
+	public AppUser findbyEmail(String email);
+}
