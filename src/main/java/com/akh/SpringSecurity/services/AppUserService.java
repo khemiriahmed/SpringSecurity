d70ad86aeb0bd794eaa4fb7832f4cf31e0cf1cee
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-13 03:12:57 +0100

    services

---
+++ src/main/java/com/akh/SpringSecurity/services/AppUserService.java
@@ -0,0 +1,34 @@
+package com.akh.SpringSecurity.services;
+
+import java.util.Optional;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.akh.SpringSecurity.models.AppUser;
+import com.akh.SpringSecurity.repositories.AppUserRepository;
+
+@Service
+public class AppUserService implements UserDetailsService {
+
+	@Autowired
+	private AppUserRepository repo;
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+	    AppUser appUser = repo.findbyEmail(email);
+	    if (appUser != null) {
+	        return  User.withUsername(appUser.getEmail())
+	          
+	            .password(appUser.getPassword())
+                .roles(appUser.getRole())
+                .build();
+	            
+
+	    } 
+	    throw new UsernameNotFoundException("User not found: " + email);
+	    }
+	}
